27cff806edf82a9bca29416bef732b02d1f1611e
LucasMartins99 <fabio.lucaslima@outlook.com>
2019-12-18 23:40:16 -0300

    Projeto Netshoes usando Redux

---
+++ Redux/src/config/ReactotronConfig.js
@@ -0,0 +1,10 @@
+import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+
+if (process.env.NODE_ENV === 'development') {
+    const tron = Reactotron.configure({ host: '192.168.0.104' })
+        .use(reactotronRedux())
+        .connect();
+    tron.clear();
+    console.tron = tron;
+}
---
+++ Redux/src/pages/Cart/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+    MdRemoveCircleOutline,
+    MdAddCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+function Cart({ cart }) {
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th> </th>
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th> </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cart.map(product => (
+                        <tr>
+                            <td>
+                                <img src={product.image} Alt={product.title} />
+                            </td>
+                            <td>
+                                <strong>{product.title}</strong>
+                                <span>{product.priceFormatted}</span>
+                            </td>
+                            <td>
+                                <div>
+                                    <button type="button">
+                                        <MdRemoveCircleOutline
+                                            size={20}
+                                            color="#7159c1"
+                                        />
+                                    </button>
+                                    <input
+                                        type="number"
+                                        readOnly
+                                        value={product.amount}
+                                    />
+                                    <button type="button">
+                                        <MdAddCircleOutline
+                                            size={20}
+                                            color="#7159c1"
+                                        />
+                                    </button>
+                                </div>
+                            </td>
+                            <td>
+                                <strong>R$260,00</strong>
+                            </td>
+                            <td>
+                                <button type="button">
+                                    <MdDelete size={20} color="#7159c1" />
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </ProductTable>
+            <footer>
+                <button type="button">Finalizar pedido</button>
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$100,29</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
+}
+const mapStateToProps = state => ({
+    cart: state.cart,
+});
+export default connect(mapStateToProps)(Cart);
